ff56c6b910adae4fd8bb568b536367f33fbb03ff
Harry Mellsop <harrymellsop@gmail.com>
2019-12-12 21:58:54 -0800

    Initial commit

---
+++ derigraph/DerigraphAtom.py
@@ -0,0 +1,5 @@
+class DerigraphAtom:
+
+    def __init__(self, value, name):
+        self.value = value
+        self.name = name
---
+++ derigraph/test-harness.py
@@ -0,0 +1,17 @@
+print("Test environment")
+
+class DerigraphComposition:
+
+    # class constructor
+    def __init__(self):
+        print("I'm a composition, look at me!")
+
+    # evaluate the entire derigraph expression
+    def evaluate(self):
+        pass
+
+    # TODO: overload relevant mathematical operators
+    # how do we structure the data internally?  needs to be a graph structure.  variables are going to need to have names, and relationships in
+    # the hierarchy.  Search tree needs to be implemented to perform sanity checking on whether or not the calculus requested is actually possible
+
+test = DerigraphComposition()
